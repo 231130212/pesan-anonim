9125392883c8eb49c9c9218e5d167609be61e3cd
Zoni <zoni.zikro@students.mikroskil.ac.id>
2024-10-06 23:41:58 +0700

    commit ketiga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pesan Anonim</h1>
+      <MessageForm />
+      <MessageList />
     </div>
   );
-}
+};
 
 export default App;
